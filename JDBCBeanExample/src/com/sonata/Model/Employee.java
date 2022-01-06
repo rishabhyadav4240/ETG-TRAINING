4b99bfb81f957dbab6a7f14826fe106e2430b2ff
rishabhyadav4240 <72446545+rishabhyadav4240@users.noreply.github.com>
2022-01-06 15:33:58 +0530

    Add files via upload

---
+++ JDBCBeanExample/src/com/sonata/Model/Employee.java
@@ -0,0 +1,27 @@
+package com.sonata.Model;
+
+public class Employee 
+{
+	private int empId;
+	private String empName;
+	private double empsal;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpsal() {
+		return empsal;
+	}
+	public void setEmpsal(double empsal) {
+		this.empsal = empsal;
+	}
+	
+}
